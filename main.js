546886957e9b3168d4ef35339b6f8769aea74632
mayzit <157802254+mayzit@users.noreply.github.com>
2024-01-26 17:43:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text", {
+    strings:["FrontEnd Developer","BackEnd Developer","VideoEditor"],
+    typeSpeed:30,
+    backSpeed:30,
+    backDelay:500,
+    loop: true
+});
+    
